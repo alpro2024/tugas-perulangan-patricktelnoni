b52621725e492563ea8c5e2019c0eeefb35075fc
patricktelnoni <patricktelnoni@users.noreply.github.com>
2024-10-13 04:30:16 +0700

    Update TugasPerulangan2.java

--- TugasPerulangan2.java
+++ TugasPerulangan2.java
@@ -5,5 +5,16 @@ public class TugasPerulangan2{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int N  = input.nextInt();
+
+        if(N < 2 || N > 100){
+            System.out.println(-1);
+            
+        }
+        else{
+            for (int i = 1; i <= N; i++) {
+                int output = i * 3;
+                System.out.println(output);
+            }
+        }
     }
 }
